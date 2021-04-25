c36fbf8e421a6b81ef789721757ee122c209c3f9
Victor Franco <vfranco.itc@gmail.com>
2021-04-24 23:23:27 -0500

    Added first project files

---
+++ var/www/html/database_iot.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Tabla con base de datos</title>
+ <style type="text/css">
+  table{
+     border-collapse: collapse;
+     width: 100%;
+     color: #CD214F;
+     font-family: monospace;
+     font-size: 25px;
+     text-align: center;
+  }
+  th{
+     background-color: #CD214F;
+     color: white;
+  }
+  tr:nth-child(even) {background-color: #f2f2f2}
+ </style>
+</head>
+<body>
+<table>
+ <tr>
+   <th>Electrovalvula I</th>
+   <th>Sensor humedad I</th>
+   <th>Temperatura (°C)</th>
+   <th>Electrovalvula II</th>
+   <th>Sensor humedad II</th>
+   <th>Nivel de agua</th>
+   <th>Luminosidad</th>
+   <th>Sensor humedad III</th>
+   <th>Fecha</th>
+ </tr>
+ <?php
+ $conn = mysqli_connect("localhost", "rasp", "pi", "cultivo");
+ if($conn-> connect_error){
+    die("conexion fallida:". $conn-> connect_error);
+ }
+ 
+ $sql = "SELECT `Electrovalvula 1`,`Sensor humedad 1`,`Temperatura`,`Electrovalvula 2`,`Sensor humedad 2`,`Nivel de agua`,`Luminosidad`,`Sensor humedad 3`,`Hora` from Sensores";
+ $result = $conn-> query($sql);
+ 
+ if($result-> num_rows > 0){
+    while($row = $result-> fetch_assoc()){
+      echo "<tr><td>".$row["Electrovalvula 1"] ."</td><td>".$row["Sensor humedad 1"]."</td><td>".$row["Temperatura"]."</td><td>".$row["Electrovalvula 2"]."</td><td>".$row["Sensor humedad 2"]."</td><td>".$row["Nivel de agua"]."</td><td>".$row["Luminosidad"]."</td><td>".$row["Sensor humedad 3"]."</td><td>".$row["Hora"]."</td></tr>";
+    }
+    echo "</table>";
+ }
+ else{
+    echo "No hay resultados";
+ }
+ 
+ $conn->close();
+ ?>
+</table>
+</body>
+</html>
+    
